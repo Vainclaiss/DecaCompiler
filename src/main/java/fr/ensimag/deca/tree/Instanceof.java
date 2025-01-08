49d1e5b6ac45e82655d99929e7683157aeb0bf9a
Matéo Charles-Mennier <Mateo.Charles--Mennier@grenoble-inp.org>
2025-01-08 18:05:18 +0100

    add(Parsing):instanceof

--- src/main/java/fr/ensimag/deca/tree/Instanceof.java
+++ src/main/java/fr/ensimag/deca/tree/Instanceof.java
@@ -1,4 +1,16 @@
 package fr.ensimag.deca.tree;
 
-public class Instanceof {
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.Register;
+
+public class Instanceof extends AbstractOpIneq{
+      public Instanceof(AbstractExpr leftOperand, AbstractExpr rightOperand) {
+            super(leftOperand, rightOperand);
+        }
+        @Override
+        protected String getOperatorName() {
+            return "instanceof";
+        }
+
 }
