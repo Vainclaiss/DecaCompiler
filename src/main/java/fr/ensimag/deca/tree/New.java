e9d4d8e10842cad24dbaa735badc4ff76752aa4c
Matéo Charles-Mennier <Mateo.Charles--Mennier@grenoble-inp.org>
2025-01-09 13:50:20 +0100

    add(Parsing) : new parse

---
+++ src/main/java/fr/ensimag/deca/tree/New.java
@@ -0,0 +1,59 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.codegen.execerrors.IOError;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.DVal;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.instructions.BOV;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.RFLOAT;
+import fr.ensimag.ima.pseudocode.instructions.RINT;
+
+import java.io.PrintStream;
+
+/**
+ *
+ * @author gl01
+ * @date 01/01/2025
+ */
+public class New extends AbstractExpr {
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+
+       //TODO je sais pas si faut faire ça ou pas
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    protected void codeExp(DecacCompiler compiler) {
+        //TODO je sais pas si faut faire ça ou pas
+    }
+
+    @Override
+    protected void codeExp(DecacCompiler compiler, int n) {
+        //TODO je sais pas si faut faire ça ou pas
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        s.print("New()");
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        // leaf node => nothing to do
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        // leaf node => nothing to do
+    }
+
+}
