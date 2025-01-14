5cec7d05ff651cc2b822712afa99eaad05642cf8
Cyprien Raballand <cyprien.raballand@grenoble-inp.org>
2025-01-14 17:35:03 +0100

    add(dead code removed by accident) : opBool and not

--- src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
@@ -20,6 +20,24 @@ public abstract class AbstractOpBool extends AbstractBinaryExpr {
     }
 
     @Override
+    protected void codeExp(DecacCompiler compiler, int n) {
+        Label e = new Label("binaryBool_eval_true");
+        String suffixe = e.getAndAddNewSuffixe();
+
+        // si l'expression est évaluée à vrai on jump a not_eval_true sinon on load 0
+        // dans Rn
+        codeGenBool(compiler, true, e);
+        compiler.addInstruction(new LOAD(0, Register.getR(n)));
+        Label skipEvalTrue = new Label("skip_eval_true");
+        skipEvalTrue.addSuffixe(suffixe);
+        compiler.addInstruction(new BRA(skipEvalTrue));
+
+        compiler.addLabel(e);
+        compiler.addInstruction(new LOAD(1, Register.getR(n)));
+        compiler.addLabel(skipEvalTrue);
+    }
+
+    @Override
     protected Type getTypeBinaryOp(DecacCompiler compiler, Type type1, Type type2) throws ContextualError {
         if (type1.isBoolean() && type2.isBoolean()) {
             return compiler.environmentType.BOOLEAN;
